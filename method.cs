986994e50eb505cfdec97c610ef207c966536871
Pradeep Sekar <133757996+pradeep-14298@users.noreply.github.com>
2023-07-25 23:22:24 +0530

    Add files via upload

---
+++ method.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace immidart
+{
+    class person
+    {
+        public void Name()
+        {
+            Console.WriteLine("Pradeep");
+
+        }
+        public void Age()
+        {
+            Console.WriteLine("25");
+        }
+
+
+        class Methodcalling
+        {
+            static void Main(string[] args)
+            {
+                person obj = new person();
+                obj.Name();
+                obj.Age();
+            }
+        }
+
+    }
+}
+/*
+Pradeep
+25
+*/
